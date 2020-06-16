9138abd8aba6ba930bd75ef7492878e3f06f1c12
gevenbly <39227699+gevenbly@users.noreply.github.com>
2020-06-16 16:19:15 -0400

    restructed the solvers
    made the full solver a stand-alone routine

--- example.py
+++ example.py
@@ -21,6 +21,25 @@ cont_order, costs, is_optimal = ncon_solver(tensors,connects,max_kept=None)
 T = ncon(tensors,connects,cont_order)
 
 
+# define a network
+chi = 4;
+chi = 10
+chi_p = 10
+u = np.random.rand(chi,chi,chi_p,chi_p)
+w = np.random.rand(chi_p,chi_p,chi)
+ham = np.random.rand(chi,chi,chi,chi,chi,chi)
+rho = np.random.rand(chi,chi,chi,chi,chi,chi)
+tensors = [u,u,w,w,w,ham,u,u,w,w,w,rho]
+connects = [[1,3,10,11],[4,7,12,13],[8,10,18],[11,12,19],[13,14,20],
+	[2,5,6,3,4,7],[1,2,9,17],[5,6,16,15],[8,9,23],[17,16,22],[15,14,21],
+	[23,22,21,18,19,20]]
+
+# solve for optimal contraction order
+cont_order, costs, is_optimal = ncon_solver(tensors,connects,max_kept=None)
+
+# contract network
+T = ncon(tensors,connects,cont_order)
+
 
 
 
--- solve_functs.py
+++ solve_functs.py
@@ -1,50 +1,31 @@
-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import numpy as np
+from typing import List, Union, Any, Tuple, Type, Optional, Sequence
+
+"""
+-----------------------------------------------------------------------------
+tn_solvers.py
+-----------------------------------------------------------------------------
+A module providing functions to solve for the optimal contraction order of 
+tensor networks.
+"""
 
 #------------------------------------------------------------------------#
-def ncon_solver(tensors,connects,max_kept=None):
-
-  # start by trying both greed algorithms
-  log_adj, costs, groups, orders = ncon_to_adj(tensors,connects)
-  order0, cost0 = greedy_size_solve(log_adj.copy())
-  order1, cost1 = greedy_cost_solve(log_adj.copy())
-  if (cost0 < cost1):
-    order_greedy = order0
-    cost_greedy = cost0
-  else:
-    order_greedy = order1
-    cost_greedy = cost1
-  
-  if (max_kept == 1):
-    # return results from greedy
-    order_was_found = False
-  else:
-    # try full algorithm (using cost_bound from greedy)
-    cost_bound = cost_greedy + 1e-4 #small fudge factor
-    N = log_adj.shape[0]
-    total_truncated = 0
-    num_samples = 1
-    order_was_found = True
-    
-    for t in range(N-1):
-      log_adj,costs,groups,orders,num_truncated = full_solve_order(log_adj,costs,groups,orders,cost_bound=cost_bound,max_kept=max_kept)
-      if (log_adj.size == 0):
-        order_was_found = False
-        break    
-      total_truncated = total_truncated + num_truncated
-      num_samples = np.append(num_samples,costs.size)
-  
-  if order_was_found:
-    cont_order = ord_to_ncon(connects,orders)
-    is_optimal = (total_truncated == 0)
-    return cont_order, costs.item(), is_optimal
-  else:
-    cont_order = ord_to_ncon(connects,order_greedy)
-    is_optimal = False
-    return cont_order, cost_greedy, is_optimal
-  
-#------------------------------------------------------------------------#
-def greedy_size_solve(log_adj):
+def greedy_size_solve(log_adj: np.ndarray):
+  """
+  Solve for the contraction order of a tensor network (encoded as a 
+  log-adjacency matrix) using a greedy algorithm that minimizes the 
+  intermediate tensor sizes. 
+  Args:
+    log_adj: matrix where element [i,j] is the log10 of the total dimension 
+      of the indices connecting ith and jth tensors.
+  Returns:
+    np.ndarray: cheapest contraction order found, specified as a sequence of 
+      binary contractions.
+    float: the cost of the network contraction, given as log10(total_FLOPS).
+  """  
   N = log_adj.shape[0]
   log_adj = log_adj.reshape(N,N)
   orders = np.zeros([2,0],dtype=int)
@@ -64,9 +45,7 @@ def greedy_size_solve(log_adj):
     # compute maximum dim of tensor in contraction 
     temp_mat = np.kron(dims,np.ones([N,1]))
     max_dim = np.maximum(temp_mat,temp_mat.T)
-    
     dim_change = (1000*(new_dims - max_dim)).astype(int)
-     # dim_change = dim_change + max(dim_change.flatten())*np.eye(N)
     
     # compute coords of minimal dim increase 
     xcoord, ycoord = np.where(dim_change == min(dim_change.flatten()))
@@ -96,7 +75,19 @@ def greedy_size_solve(log_adj):
   return orders, costs
 
 #------------------------------------------------------------------------#
-def greedy_cost_solve(log_adj):
+def greedy_cost_solve(log_adj: np.ndarray):
+  """
+  Solve for the contraction order of a tensor network (encoded as a 
+  log-adjacency matrix) using a greedy algorithm that minimizes the 
+  contraction cost at each step. 
+  Args:
+    log_adj: matrix where element [i,j] is the log10 of the total dimension 
+      of the indices connecting ith and jth tensors.
+  Returns:
+    np.ndarray: cheapest contraction order found, specified as a sequence of 
+      binary contractions.
+    float: the cost of the network contraction, given as log10(total_FLOPS).
+  """  
   N = log_adj.shape[0]
   log_adj = log_adj.reshape(N,N)
   orders = np.zeros([2,0],dtype=int)
@@ -135,7 +126,92 @@ def greedy_cost_solve(log_adj):
   return orders, costs
 
 #------------------------------------------------------------------------#
-def full_solve_order(log_adj,costs,groups,orders,cost_bound=None,max_kept=None,allow_outer=False):
+def full_solve_complete(log_adj: np.ndarray,
+                        cost_bound: Optional[int] = None,
+                        max_kept: Optional[int] = None):
+  """
+  Solve for optimal contraction path of a network encoded as a log-adjacency 
+  matrix via a full search. 
+  Args:
+    log_adj: the log10 of the adjacency matrix of the network.
+    cost_bound: upper cost threshold for discarding paths, in log10(FLOPS).
+    max_kept: bound for the total number of paths to retain.
+  Returns:
+    np.ndarray: the cheapest contraction order found.
+    float: the cost of the network contraction, given as log10(total_FLOPS).
+    bool: specifies if contraction order is guaranteed optimal.
+  """  
+  # start by trying both greedy algorithms
+  order0, cost0 = greedy_size_solve(log_adj.copy())
+  order1, cost1 = greedy_cost_solve(log_adj.copy())
+  if (cost0 < cost1):
+    order_greedy = order0
+    cost_greedy = cost0
+  else:
+    order_greedy = order1
+    cost_greedy = cost1
+  
+  if (max_kept == 1):
+    # return results from greedy
+    order_was_found = False
+  else:
+    # initialize arrays
+    N = log_adj.shape[0]
+    costs = np.zeros([1,1])
+    groups =  np.array(2**np.arange(N),dtype=np.uint64).reshape(N,1)
+    orders = np.zeros([2,0,1],dtype=int)
+    
+    # try full algorithm (using cost_bound from greedy)
+    cost_bound = cost_greedy + 1e-4 #small fudge factor
+    total_truncated = 0
+    order_was_found = True
+    for t in range(N-1):
+      log_adj,costs,groups,orders,num_truncated = full_solve_single(log_adj,costs,groups,orders,cost_bound=cost_bound,max_kept=max_kept)
+      if (log_adj.size == 0):
+        # no paths found within the cost-bound
+        order_was_found = False
+        break    
+      total_truncated = total_truncated + num_truncated
+  
+  if order_was_found:
+    # return result from full algorithm
+    is_optimal = (total_truncated == 0)
+    return orders, costs.item(), is_optimal
+  else:
+    # return result from greedy algorithm
+    is_optimal = False
+    return order_greedy, cost_greedy, is_optimal
+
+#------------------------------------------------------------------------#
+def full_solve_single(log_adj: np.ndarray,
+                     costs: np.ndarray,
+                     groups: np.ndarray,
+                     orders: np.ndarray,
+                     cost_bound: Optional[int] = None,
+                     max_kept: Optional[int] = None,
+                     allow_outer: Optional[bool] = False):
+  """
+  Solve for the most-likely contraction step given a set of networks encoded 
+  as log-adjacency matrices. Uses an algorithm that searches multiple (or,
+  potentially, all viable paths) as to minimize the total contraction cost. 
+  Args:
+    log_adj: an np.ndarray of log-adjacency matrices of dim (N,N,m), with `N`
+      the number of tensors and `m` the number of (intermediate) networks.
+    costs: np.ndarray of length `m` detailing to prior cost of each network. 
+    groups: np.ndarray of dim (N,m) providing an id-tag for each network, 
+      based on a power-2 encoding.
+    orders: np.ndarray of dim (2,t,m) detailing the pairwise contraction 
+      history of each network from the previous `t` contraction steps.
+    cost_bound: upper cost threshold for discarding paths, in log10(FLOPS).
+    max_kept: bound for the total number of paths to retain.
+    allow_outer: sets whether outer products are allowed.
+  Returns:
+    np.ndarray: new set of `log_adj` matrices.
+    np.ndarray: new set of `costs`.
+    np.ndarray: new set of `groups`.
+    np.ndarray: new set of `orders`.
+    int: total number of potentially viable paths that were trimmed.
+  """  
   
   # set threshold required to trigger compression routine
   if max_kept is None:
@@ -159,9 +235,13 @@ def full_solve_order(log_adj,costs,groups,orders,cost_bound=None,max_kept=None,a
   for i in range(1,N):
     for j in range(i):
       
-      # only attempt non-trivial contractions
-      new_pos = np.flatnonzero(log_adj[j,i,:] > 0) 
-      num_kept = len(new_pos)
+      if not allow_outer:
+        # only attempt non-trivial contractions
+        new_pos = np.flatnonzero(log_adj[j,i,:] > 0) 
+        num_kept = len(new_pos)
+      else:
+        new_pos = np.arange(log_adj.shape[2])
+        num_kept = len(new_pos)
       
       if num_kept > 0:
         only_outer_exist = False
@@ -239,7 +319,7 @@ def full_solve_order(log_adj,costs,groups,orders,cost_bound=None,max_kept=None,a
   
   if only_outer_exist: # network contains only outer products
     # re-solve with outer products enabled
-    return full_solve_order(log_adj,costs,groups,orders,cost_bound=cost_bound,max_kept=max_kept,allow_outer=True)
+    return full_solve_single(log_adj,costs,groups,orders,cost_bound=cost_bound,max_kept=max_kept,allow_outer=True)
   else: 
     # compress outputs
     temp_pos = reduce_nets(final_costs,final_groups,final_stable)[0]
@@ -251,21 +331,37 @@ def full_solve_order(log_adj,costs,groups,orders,cost_bound=None,max_kept=None,a
     return final_adj,final_costs,final_groups,final_orders,total_truncated
         
 #------------------------------------------------------------------------#
-def reduce_nets(costs,groups,stable,max_kept=None):
+def reduce_nets(costs: np.ndarray,
+                groups: np.ndarray,
+                stable: np.ndarray,
+                max_kept: Optional[int] = None):
+  """
+  Reduce from `m` starting paths smaller number of paths by first (i) 
+  identifying any equivalent networks then (ii) trimming the most expensive 
+  paths.
+  Args:
+    costs: np.ndarray of length `m` detailing to prior cost of each network. 
+    groups: np.ndarray of dim (N,m) providing an id-tag for each network, 
+      based on a power-2 encoding.
+    stable: np.ndarray of dim (m) denoting which paths were size-stable.
+    max_kept: bound for the total number of paths to retain.
+  Returns:
+    np.ndarray: index positions of the kept paths.
+    int: total number of potentially viable paths that were trimmed.
+  """  
   
   # sort according to the costs
   new_pos = np.argsort(costs).flatten();
 
-  # identify identical networks
+  # identify and remove identical networks
   temp_pos = np.unique(groups[:,new_pos],return_index=True,axis=1)[1]
   orig_kept = len(temp_pos)
-  
   new_pos = new_pos[temp_pos]
   num_truncated = 0;
   
   if max_kept is not None:
     if (orig_kept > max_kept):
-      # re-sort according to the costs (np.unique breaks cost ordering
+      # re-sort according to the costs 
       cost_order = np.argsort(costs[:,new_pos]).flatten()
       new_pos = new_pos[cost_order]
       
@@ -276,23 +372,61 @@ def reduce_nets(costs,groups,stable,max_kept=None):
       
       stable_pos = np.flatnonzero(stable[0,new_pos[num_cheapest:]])
       temp_pos = np.concatenate((np.arange(num_cheapest),stable_pos[:min(len(stable_pos),num_stable)] + num_cheapest),axis=0)
-      
       new_pos = new_pos[temp_pos]
       num_truncated = orig_kept-len(new_pos)
       
   return new_pos, num_truncated
 
 #------------------------------------------------------------------------#
-def ncon_to_adj(tensors,connects):
+def ncon_solver(tensors: List[np.ndarray],
+                connects: List[List[int]],
+                max_kept: Optional[int] = None):
+  """
+  Solve for the contraction order of a tensor network (encoded in the `ncon` 
+  syntax) that minimizes the computational cost. 
+  Args:
+    tensors: list of the tensors in the network.
+    connects: list of the tensor connections (in standard `ncon` format).
+    max_kept: maximum number of contraction paths to search at each step. 
+  Returns:
+    np.ndarray: the cheapest contraction order found (in ncon format).
+    float: the cost of the network contraction, given as log10(total_FLOPS).
+    bool: specifies if contraction order is guaranteed optimal.
+  """
+  # build log-adjacency matrix
+  log_adj = ncon_to_adj(tensors,connects)
+  
+  # run search algorithm
+  order, costs, is_optimal = full_solve_complete(log_adj,max_kept=max_kept)
+  
+  # put contraction order back into ncon format
+  cont_order = ord_to_ncon(connects,order)
+  
+  return cont_order, costs, is_optimal
+
+#------------------------------------------------------------------------#
+def ncon_to_adj(tensors: List[np.ndarray],
+                connects: List[List[int]]):
+  """
+  Create a log-adjacency matrix for a network from the `ncon` syntax. 
+  Args:
+    tensors: list of the tensors in the network.
+    connects: list of the tensor connections (in standard `ncon` format).
+  Returns:
+    np.ndarray: the log-adjacency matrix.
+  """
+  # process inputs  
   N = len(connects)
   ranks = [len(connects[i]) for i in range(N)]
   flat_connects = np.hstack([connects[i] for i in range(N)])
   tensor_counter = np.hstack([i*np.ones(ranks[i],dtype=int) for i in range(N)])
   index_counter = np.hstack([np.arange(ranks[i]) for i in range(N)])
   
+  # build log-adjacency index by index  
   log_adj = np.zeros([N,N]);
   unique_connects = np.unique(flat_connects)
   for i in range(len(unique_connects)):
+      # identify tensor/index location of each edge
       t_loc = tensor_counter[flat_connects == unique_connects[i]]
       i_loc = index_counter[flat_connects == unique_connects[i]]
       if len(i_loc) == 1: # external index
@@ -302,25 +436,32 @@ def ncon_to_adj(tensors,connects):
         log_adj[t_loc[1],t_loc[0]] = log_adj[t_loc[1],t_loc[0]] + np.log10(tensors[t_loc[0]].shape[i_loc[0]])
    
   log_adj = log_adj.reshape(N,N,1)
-  costs = np.zeros([1,1])
-  groups =  np.array(2**np.arange(N),dtype=np.uint64).reshape(N,1)
-  orders = np.zeros([2,0,1],dtype=int)
   
-  return log_adj,costs,groups,orders
+  return log_adj
 
 #------------------------------------------------------------------------#
-def ord_to_ncon(connects,orders):
+def ord_to_ncon(connects: List[List[int]],
+                orders: np.ndarray):
+  """
+  Produces a `ncon` compatible index contraction order. 
+  Args:
+    connects: list of the tensor connections (in standard `ncon` format).
+    orders: array of dim (2,N-1) specifying the set of N-1 pairwise 
+      tensor contractions.
+  Returns:
+    np.ndarray: the contraction order (in `ncon` format).
+  """
   N = orders.size//2
   orders = orders.reshape(2,N)
   new_connects = [connects[i] for i in range(len(connects))]
   cont_order = np.zeros([0],dtype=int)
   for i in range(N):  
+    # find common indices between tensor pair
     cont_many, A_cont, B_cont = np.intersect1d(new_connects[orders[0,i]],new_connects[orders[1,i]], assume_unique=True, return_indices=True)
     temp_connects = np.append(np.delete(new_connects[orders[0,i]], A_cont), np.delete(new_connects[orders[1,i]], B_cont))
-  
-    common_inds = np.intersect1d(new_connects[orders[0,i]],new_connects[orders[1,i]])
-    cont_order = np.concatenate((cont_order,common_inds),axis=0)
+    cont_order = np.concatenate((cont_order,cont_many),axis=0)
     
+    # build new set of connects
     del new_connects[orders[1,i]]
     new_connects[orders[0,i]] = temp_connects
 
